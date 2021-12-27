c4285ceccb14fb148bac3cb56df6c5c383cbd5bb
VitalyaVpn <vitalya05111994@gmail.com>
2021-12-28 00:33:55 +0300

    add: pause time, membership check, catch errors (current)

--- stephandlers/employeeStepTwo.ts
+++ stephandlers/employeeStepTwo.ts
@@ -1,124 +1,141 @@
 import {Composer, Markup} from "telegraf";
 import {MyContext} from "../types";
-import {db} from "../google/firestore";
-import {dateNow} from "./employeeStepOne";
-import {accessSpreadSheet, appendTask, updateShift, updateTask} from "../google/spreadsheet";
-import {makeButtons, trigger} from "../functions";
+import { appendTask, updateShift, updateTask} from "../google/spreadsheet";
+import {dateString, makeButtons, trigger} from "../functions";
 
 export const employeeStepTwo = new Composer<MyContext>()
 
 
 employeeStepTwo.action(trigger, async (ctx) => {
-    await ctx.answerCbQuery()
-    await ctx.deleteMessage()
-    if(ctx.scene.session.taskMessage){
-        await ctx.deleteMessage(ctx.scene.session.taskMessage)
-    }
-
-    const message = await ctx.reply('Выберите задачу из списка или просто напишите её сообщением', Markup.inlineKeyboard(makeButtons(ctx.scene.session.tasks)))
-
-
-    const id = message.chat.id
-    if (ctx.scene.session.currentTask) {
-
-        const task = ctx.scene.session.tasks[+ctx.match[0]]
-        await updateTask(id, ctx.scene.session.currentTask?.column)
-        const column = await appendTask(id, task)
-        if (column) {
-            ctx.scene.session.currentTask = {
-                name: task,
-                column,
-            }
-            const taskMessage = await ctx.reply(`Сейчас выполняется задача: ${ctx.scene.session.currentTask.name}`)
-            ctx.scene.session.taskMessage = taskMessage.message_id
+    try {
+        await ctx.answerCbQuery()
+        await ctx.deleteMessage()
+        if(ctx.scene.session.taskMessage){
+            await ctx.deleteMessage(ctx.scene.session.taskMessage)
         }
 
-    }
-    if (!ctx.scene.session.currentTask) {
-        const task = ctx.scene.session.tasks[+ctx.match[0]]
-        const column = await appendTask(id, task)
-        if (column) {
-            ctx.scene.session.currentTask = {
-                name: task,
-                column,
+        const message = await ctx.reply('Выберите задачу из списка или просто напишите её сообщением', Markup.inlineKeyboard(makeButtons(ctx.scene.session.tasks)))
+
+
+        const id = message.chat.id
+        if (ctx.scene.session.currentTask) {
+
+            const task = ctx.scene.session.tasks[+ctx.match[0]]
+
+            await updateTask(id, ctx.scene.session.currentTask?.column, ctx.scene.session.table,  ctx.scene.session.taskStart)
+            ctx.scene.session.taskStart = new Date()
+            const column = await appendTask(id, task, ctx.scene.session.name,ctx.scene.session.table,  ctx.scene.session.taskStart)
+            if (column) {
+                ctx.scene.session.currentTask = {
+                    name: task,
+                    column,
+                }
+                const taskMessage = await ctx.reply(`Сейчас выполняется задача: ${ctx.scene.session.currentTask.name}`)
+                ctx.scene.session.taskMessage = taskMessage.message_id
             }
-            const taskMessage = await ctx.reply(`Сейчас выполняется задача: ${ctx.scene.session.currentTask.name}`)
-            ctx.scene.session.taskMessage = taskMessage.message_id
+
+        }
+        if (!ctx.scene.session.currentTask) {
+            const task = ctx.scene.session.tasks[+ctx.match[0]]
+            ctx.scene.session.taskStart = new Date()
+            const column = await appendTask(id, task, ctx.scene.session.name, ctx.scene.session.table,  ctx.scene.session.taskStart)
+            if (column) {
+                ctx.scene.session.currentTask = {
+                    name: task,
+                    column,
+                }
+                const taskMessage = await ctx.reply(`Сейчас выполняется задача: ${ctx.scene.session.currentTask.name}`)
+                ctx.scene.session.taskMessage = taskMessage.message_id
+            }
+
         }
 
+        ctx.scene.session.lastMessage = message.message_id
+        return ctx.wizard.selectStep(1)
     }
-
-
-    ctx.scene.session.lastMessage = message.message_id
-    return ctx.wizard.selectStep(1)
+    catch (err) {
+        console.log(err)
+    }
 })
 
 employeeStepTwo.hears(['Закончить смену'], async (ctx) => {
-    // await db.collection('users').doc(ctx.message.from.id.toString()).collection('workday').doc(dateNow()).update({
-    //     end: Date.now()
-    // })
-
-    await ctx.deleteMessage(ctx.scene.session.lastMessage)
-    await ctx.deleteMessage(ctx.scene.session.endMessage)
+    try {
+        await ctx.deleteMessage(ctx.scene.session.lastMessage)
+        await ctx.deleteMessage(ctx.scene.session.endMessage)
 
 
-    await ctx.reply('До завтра', Markup.keyboard([['Начать смену']]).resize(true))
-    if (ctx.scene.session.currentTask) {
-        await updateTask(ctx.message.from.id, ctx.scene.session.currentTask?.column,)
-        ctx.scene.session.currentTask = null
+        await ctx.reply('До завтра', Markup.keyboard([['Начать смену']]).resize(true))
+        if (ctx.scene.session.currentTask) {
+            await updateTask(ctx.message.from.id, ctx.scene.session.currentTask?.column, ctx.scene.session.table,  ctx.scene.session.taskStart)
+            ctx.scene.session.currentTask = null
+        }
+        const pause = dateString(ctx.scene.session.pauseTime)
+        await updateShift(ctx.message.from.id, ctx.scene.session.column, ctx.scene.session.table, pause)
+        return ctx.scene.leave()
     }
-    await updateShift(ctx.message.from.id, ctx.scene.session.column)
-    console.log(ctx.scene.session.pauseTime)
-    return ctx.scene.leave()
+    catch (err) {
+        console.log(err)
+    }
 })
 
 employeeStepTwo.hears(['Пауза'], async (ctx) => {
-    ctx.scene.session.pauseStart = Date.now()
-    await ctx.deleteMessage(ctx.scene.session.lastMessage)
-    const message = await ctx.reply('Смена на паузе', Markup.keyboard([['Закончить смену', 'Возобновить']]).resize(true))
-    ctx.scene.session.lastMessage = message.message_id
-    return ctx.wizard.selectStep(2)
+    try {
+        ctx.scene.session.pauseStart = Date.now()
+        await ctx.deleteMessage(ctx.scene.session.lastMessage)
+        const message = await ctx.reply('Смена на паузе', Markup.keyboard([['Закончить смену', 'Возобновить']]).resize(true))
+        ctx.scene.session.lastMessage = message.message_id
+        return ctx.wizard.selectStep(2)
+    }
+    catch (err) {
+        console.log(err)
+    }
 })
 
 employeeStepTwo.on('text', async (ctx) => {
+    try {
+        if(ctx.scene.session.taskMessage){
+            await ctx.deleteMessage(ctx.scene.session.taskMessage)
+        }
+        await ctx.deleteMessage(ctx.scene.session.lastMessage)
+        const message = await ctx.reply('Выберите задачу из списка или просто напишите её сообщением', Markup.inlineKeyboard(makeButtons(ctx.scene.session.tasks)))
 
-    if(ctx.scene.session.taskMessage){
-        await ctx.deleteMessage(ctx.scene.session.taskMessage)
-    }
-    await ctx.deleteMessage(ctx.scene.session.lastMessage)
-    const message = await ctx.reply('Выберите задачу из списка или просто напишите её сообщением', Markup.inlineKeyboard(makeButtons(ctx.scene.session.tasks)))
-
-    const id = message.chat.id
-    if (ctx.scene.session.currentTask) {
-        const task = ctx.message.text
-        await updateTask(id, ctx.scene.session.currentTask?.column)
-        const column = await appendTask(id, task)
-        if (column) {
-            ctx.scene.session.currentTask = {
-                name: task,
-                column,
+        const id = message.chat.id
+        if (ctx.scene.session.currentTask) {
+            const task = ctx.message.text
+            await updateTask(id, ctx.scene.session.currentTask?.column, ctx.scene.session.table, ctx.scene.session.taskStart)
+            ctx.scene.session.taskStart = new Date()
+            const column = await appendTask(id, task, ctx.scene.session.name, ctx.scene.session.table,  ctx.scene.session.taskStart)
+            if (column) {
+                ctx.scene.session.currentTask = {
+                    name: task,
+                    column,
+                }
+                const taskMessage = await ctx.reply(`Сейчас выполняется задача: ${ctx.scene.session.currentTask.name}`)
+                ctx.scene.session.taskMessage = taskMessage.message_id
             }
-            const taskMessage = await ctx.reply(`Сейчас выполняется задача: ${ctx.scene.session.currentTask.name}`)
-            ctx.scene.session.taskMessage = taskMessage.message_id
+
+        }
+        if (!ctx.scene.session.currentTask) {
+            const task = ctx.message.text
+            ctx.scene.session.taskStart = new Date()
+            const column = await appendTask(id, task, ctx.scene.session.name, ctx.scene.session.table,  ctx.scene.session.taskStart)
+            if (column) {
+                ctx.scene.session.currentTask = {
+                    name: task,
+                    column,
+                }
+                const taskMessage = await ctx.reply(`Сейчас выполняется задача: ${ctx.scene.session.currentTask.name}`)
+                ctx.scene.session.taskMessage = taskMessage.message_id
+            }
+
         }
 
+        ctx.scene.session.lastMessage = message.message_id
+        return ctx.wizard.selectStep(1)
     }
-    if (!ctx.scene.session.currentTask) {
-        const task = ctx.message.text
-        const column = await appendTask(id, task)
-        if (column) {
-            ctx.scene.session.currentTask = {
-                name: task,
-                column,
-            }
-            const taskMessage = await ctx.reply(`Сейчас выполняется задача: ${ctx.scene.session.currentTask.name}`)
-            ctx.scene.session.taskMessage = taskMessage.message_id
-        }
-
+    catch (err) {
+        console.log(err)
     }
-
-    ctx.scene.session.lastMessage = message.message_id
-    return ctx.wizard.selectStep(1)
 
 })
 
